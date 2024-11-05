a0407084db7bff5f134edf5c9557c10ae6424a23
Tinclon <clinton.h.nielsen@gmail.com>
2024-11-04 17:16:06 -0700

    Update bitcoin-tracker.js

--- bitcoin-tracker.js
+++ bitcoin-tracker.js
@@ -8,19 +8,20 @@ import wallets from "./wallets/wallets.js"
 // export default {
 //     "A": {
 //         "addresses": [
-//             "bc1q6xhy3rn09hkrqhzlxqgc40cetutes530axtvda",
-//             "bc1q8cnuhluv6sa3fr9h7k0htfqmyctdsl8xwmnfa2",
-//             "bc1qgk4l74uef9aayc3rt69gh2gj4sf8grafmpcvxw",
-//             "bc1q0vmp5a4alp0dz287wrgz9uspwdt398hhxpr8ua",
-//             "bc1qcxfk4g3mcgs0hvh4qazw24ykvtv4fq6teqmq5p",
-//             "bc1qz8wv4a35l6wpk69wuwgcwzgayeyr4t7lx7vaqx",
-//             "bc1qyuyc6yvtpfex3t4whgzrw6lcfxfe8625t43a3z",
+//             "bc1qtq2my0lz6425ch7jnahtnvxa2uhns0a4jnc96z",
+//             "bc1qkzqhuwsuzrw2pyuchw3czwff94vj2q5und9qp6",
+//             "bc1qskr0vz624f267ywm4jms2pd3n67dj7d6cyry48",
+//             "bc1qedee3qm47wmej29e9vwrj2rg02pfh5wzsu680c",
+//             "bc1qg68j4pjg5knye0zuy7g0yk9ap4evcmdpzndmps",
+//             "bc1qhkz0utxq7gr0ql0ya6m33c53zgxtzqwt8dlmph",
+//             "bc1q86lw5n9m2fut86aqy6ltghdnuandatze32qrxl",
 //         ]
 //     },
 
 (async () => {
 
-    const delay = (delay_ms) => new Promise((_) => setTimeout(_, delay_ms));
+    const delayPromise = (delay_ms) => new Promise(_ => setTimeout(_, delay_ms));
+    const delayFunction = (delay_ms) => value => new Promise(resolve => setTimeout(() => resolve(value), delay_ms));
 
     let numAddresses = 0;
     let fetchedAddresses = 0;
@@ -29,23 +30,24 @@ import wallets from "./wallets/wallets.js"
     }
 
     for (const wallet in wallets) {
-        const promises = wallets[wallet].addresses.map(address =>
-            fetch(`https://blockstream.info/api/address/${address}`)
-                .then(response => {
-                    if (response.status !== 200) {
-                        console.error(response.status);
-                        ++fetchedAddresses;
-                        return {chain_stats: {funded_txo_sum: 0, spent_txo_sum: 0}};
-                    }
-                    console.error(`Retrieved ${++fetchedAddresses} of ${numAddresses}`);
-                    return response.json()
-                })
-                .then(json => json.chain_stats.funded_txo_sum - json.chain_stats.spent_txo_sum)
-                .then(balance => balance / 100000000));
+        const promises = wallets[wallet].addresses.map((address, index) => Promise.resolve()
+            .then(delayFunction(1000 * index)).then(() =>
+                fetch(`https://blockstream.info/api/address/${address}`)
+                    .then(response => {
+                        if (response.status !== 200) {
+                            console.error(response.status);
+                            ++fetchedAddresses;
+                            return {chain_stats: {funded_txo_sum: 0, spent_txo_sum: 0}};
+                        }
+                        console.error(`Retrieved ${++fetchedAddresses} of ${numAddresses}`);
+                        return response.json()
+                    })
+                    .then(json => json.chain_stats.funded_txo_sum - json.chain_stats.spent_txo_sum)
+                    .then(balance => balance / 100000000)));
 
         wallets[wallet].total = (await Promise.all(promises)).reduce((acc, cur) => acc + cur, 0)
 
-        await delay(100);
+        await delayPromise(1000);
     }
 
     console.error();
@@ -59,7 +61,3 @@ import wallets from "./wallets/wallets.js"
     console.error(chalk.blue.bold("Total: ₿"), total);
     console.error();
 })();
-
-
-
-
